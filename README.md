ff94b4134a8b9b61cc814af9cfcd9c2fae85b4df
Jeremy Shipman <jeremy@burnbright.net>
2014-02-12 11:49:27 +1300

    Initial code import

---
+++ README.md
@@ -0,0 +1,30 @@
+# SilverStripe Shop Geocoding
+
+Adds geocoding support to the shop:
+
+ * Work out address coordinates, based on entered address.
+
+Make use of the [geocoder-php/Geocoder](https://github.com/geocoder-php/Geocoder) library.
+
+## Configuration
+
+A default configuration is provided, but you can define your own.
+
+In _config.php:
+```php
+$geocoder = new \Geocoder\Geocoder();
+$adapter  = new \Geocoder\HttpAdapter\CurlHttpAdapter();
+$geocoder->registerProvider(
+	new \Geocoder\Provider\ChainProvider(array(
+		new \Geocoder\Provider\FreeGeoIpProvider($adapter),
+		new \Geocoder\Provider\HostIpProvider($adapter),
+		new \Geocoder\Provider\GoogleMapsProvider($adapter)
+	))
+);
+AddressGeocoding::set_geocoder($geocoder);
+```
+
+## TODO
+
+Work out address from map coordinates.
+
---
+++ code/AddressGeocoding.php
@@ -0,0 +1,51 @@
+<?php
+
+class AddressGeocoding extends DataExtension{
+
+	private static $inst;
+
+	/**
+	 * Get the configured geocoder.
+	 * Configures freegeoip, hostip, and googlemaps providers by default.
+	 * @return [type] [description]
+	 */
+	private static function get_geocoder() {
+		if(self::$inst){
+			return self::$inst;
+		}
+		$geocoder = new \Geocoder\Geocoder();
+		$adapter  = new \Geocoder\HttpAdapter\CurlHttpAdapter();
+		$geocoder->registerProvider(
+			new \Geocoder\Provider\ChainProvider(array(
+				new \Geocoder\Provider\FreeGeoIpProvider($adapter),
+				new \Geocoder\Provider\HostIpProvider($adapter),
+				new \Geocoder\Provider\GoogleMapsProvider($adapter)
+			))
+		);
+
+		return self::$inst = $geocoder;
+	}
+
+	public function set_geocoder(\Geocoder\Geocoder $geocoder) {
+		self::$inst = $geocoder;
+	}
+	
+	function onBeforeWrite() {
+		if(!$this->owner->Latitude && !$this->owner->Longitude){
+			$this->geocodeAddress();
+		}
+	}
+
+	function geocodeAddress() {
+		//TODO: check if address is valid
+		$geocoder = self::get_geocoder();
+		try {
+			$geocoded = $geocoder->geocode($this->owner->toString());
+			$this->owner->Latitude = $geocoded->getLatitude();
+			$this->owner->Longitude = $geocoded->getLongitude();
+		} catch (Exception $e) {
+			SS_Log::log($e, SS_Log::ERR);
+		}
+	}
+
+}
