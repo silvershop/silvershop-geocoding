ea485ebc278b03c5c72aad20b3707a757e7b6553
helpfulrobot <chris@silverstripe.com>
2016-01-01 05:23:35 +1300

    Converted to PSR-2

--- code/CheckoutStep_AddressLocationFallback.php
+++ code/CheckoutStep_AddressLocationFallback.php
@@ -3,66 +3,70 @@
 /**
  * A fallback to use if geocoding fails to locate address.
  */
-class CheckoutStep_AddressLocationFallback extends CheckoutStep{
-	
-	private static $allowed_actions = array(
-		"addresslocation",
-		"AddressLocationForm"
-	);
+class CheckoutStep_AddressLocationFallback extends CheckoutStep
+{
+    
+    private static $allowed_actions = array(
+        "addresslocation",
+        "AddressLocationForm"
+    );
 
-	function addresslocation() {
-		$shippingaddress = $this->getShippingAddress();
-		//TODO: verify shipping address exists
-		if((int)$shippingaddress->Latitude && (int)$shippingaddress->Longitude){
-			return $this->owner->redirect($this->NextStepLink());
-		}
-		$form = $this->AddressLocationForm();
+    public function addresslocation()
+    {
+        $shippingaddress = $this->getShippingAddress();
+        //TODO: verify shipping address exists
+        if ((int)$shippingaddress->Latitude && (int)$shippingaddress->Longitude) {
+            return $this->owner->redirect($this->NextStepLink());
+        }
+        $form = $this->AddressLocationForm();
 
-		return array(
-			'OrderForm' => $form
-		);
-	}
+        return array(
+            'OrderForm' => $form
+        );
+    }
 
-	function AddressLocationForm(){
-		$shippingaddress = $this->getShippingAddress();
+    public function AddressLocationForm()
+    {
+        $shippingaddress = $this->getShippingAddress();
 
-		$config = array(
-			'fieldNames' => array(
-				'lat' => 'Latitude',
-				'lng' => 'Longitude'
-			),
-			'coords' => array(
-				Address::config()->mapdefaults['latitude'],
-				Address::config()->mapdefaults['longitude']
-			),
-			'map' => array(
-				'zoom' => Address::config()->mapdefaults['zoom']
-			),
-			'showSearchBox' => false
-		);
-		$fields = new FieldList(
-			LiteralField::create("locationneededmessage", "<p class=\"message warning\">We could not automatically determine your shipping location. Please find and click the exact location on the map:</p>"),
-			GoogleMapField::create($shippingaddress, "Location", $config)
-				->setDescription("Please click the exact location of your address")
-		);
-		$actions = new FieldList(
-			new FormAction("setAddressLocation", "Continue")
-		);
+        $config = array(
+            'fieldNames' => array(
+                'lat' => 'Latitude',
+                'lng' => 'Longitude'
+            ),
+            'coords' => array(
+                Address::config()->mapdefaults['latitude'],
+                Address::config()->mapdefaults['longitude']
+            ),
+            'map' => array(
+                'zoom' => Address::config()->mapdefaults['zoom']
+            ),
+            'showSearchBox' => false
+        );
+        $fields = new FieldList(
+            LiteralField::create("locationneededmessage", "<p class=\"message warning\">We could not automatically determine your shipping location. Please find and click the exact location on the map:</p>"),
+            GoogleMapField::create($shippingaddress, "Location", $config)
+                ->setDescription("Please click the exact location of your address")
+        );
+        $actions = new FieldList(
+            new FormAction("setAddressLocation", "Continue")
+        );
 
-		$form = new Form($this->owner, "AddressLocationForm",$fields, $actions);
-		return $form;
-	}
+        $form = new Form($this->owner, "AddressLocationForm", $fields, $actions);
+        return $form;
+    }
 
-	function setAddressLocation($data, $form) {
-		$shippingaddress = $this->getShippingAddress();
-		$form->saveInto($shippingaddress);
-		$shippingaddress->write();
+    public function setAddressLocation($data, $form)
+    {
+        $shippingaddress = $this->getShippingAddress();
+        $form->saveInto($shippingaddress);
+        $shippingaddress->write();
 
-		return $this->owner->redirect($this->NextStepLink());
-	}
+        return $this->owner->redirect($this->NextStepLink());
+    }
 
-	protected function getShippingAddress(){
-		return ShoppingCart::singleton()->current()->getShippingAddress();
-	}
-
+    protected function getShippingAddress()
+    {
+        return ShoppingCart::singleton()->current()->getShippingAddress();
+    }
 }
--- tests/GeocodingTest.php
+++ tests/GeocodingTest.php
@@ -1,22 +1,24 @@
 <?php
 
-class GeocodingTest extends SapphireTest{
+class GeocodingTest extends SapphireTest
+{
 
-	public static $fixture_file = 'shop_geocoding/tests/addresses.yml';
-	
-	function testAddressModel() {
-		$address = $this->objFromFixture("Address", "address1");
-		$this->assertEquals(174.77908, $address->Longitude);
-		$this->assertEquals(-41.292915, $address->Latitude);
-	}
+    public static $fixture_file = 'shop_geocoding/tests/addresses.yml';
+    
+    public function testAddressModel()
+    {
+        $address = $this->objFromFixture("Address", "address1");
+        $this->assertEquals(174.77908, $address->Longitude);
+        $this->assertEquals(-41.292915, $address->Latitude);
+    }
 
-	function testAddressDistanceTo() {
-		$from = $this->objFromFixture("Address", "address1");
-		$to = $this->objFromFixture("Address", "address2");
-		$this->assertEquals(0, $from->distanceTo($from));
-		$this->assertEquals(0, $to->distanceTo($to));
-		$this->assertEquals(494.42414833321, $from->distanceTo($to));
-		$this->assertEquals(494.42414833321, $to->distanceTo($from));
-	}
-
+    public function testAddressDistanceTo()
+    {
+        $from = $this->objFromFixture("Address", "address1");
+        $to = $this->objFromFixture("Address", "address2");
+        $this->assertEquals(0, $from->distanceTo($from));
+        $this->assertEquals(0, $to->distanceTo($to));
+        $this->assertEquals(494.42414833321, $from->distanceTo($to));
+        $this->assertEquals(494.42414833321, $to->distanceTo($from));
+    }
 }
